dae4336728e068482e1869c3e2941d319e9ff056
ritankar pal <ritankar0425@gmail.com>
2023-12-02 02:08:08 -0500

    HappyNo

--- src/in/dsa/CyclicLL.java
+++ src/in/dsa/CyclicLL.java
@@ -35,4 +35,74 @@ public class CyclicLL {
 		
 	}
 	
+	public int lengthOfCycle(Node<Integer> head) {
+		
+		Node<Integer> fast = head;
+		Node<Integer> slow = head;
+		
+		int count = 0;
+		
+		while(fast != null && fast.next != null) {
+			
+			fast = fast.next.next;
+			slow = slow.next;
+			
+			if(fast == slow) {
+				
+				do {
+					slow = slow.next;
+					count++;
+				}
+				while(slow != fast);
+			}
+		}
+		
+		return count;
+	}
+	
+	public Node<Integer> cycleStart(Node<Integer> head){
+		
+		Node<Integer> fast = head;
+		Node<Integer> slow = head;
+		
+		int length = 0;
+		
+		//Finding the length of the cycle::
+		while(fast != null && fast.next != null) {
+			
+			fast = fast.next.next;
+			slow = slow.next;
+			
+			if(fast == slow) {
+				
+				do {
+					slow = slow.next;
+					length++;
+				}
+				while(slow != fast);
+			}
+		}
+		
+		if(length == 0) return null;
+		
+		Node<Integer> s = head;
+		Node<Integer> f = head;
+		
+		//Moving s length times::
+		while(length > 0) {
+			s = s.next;
+			length--;
+		}
+		
+		//Moving f and s until they meet:
+		while(f != s) {
+			f = f.next;
+			s = s.next;
+		}
+		
+		//returning either s or f::
+		return s;
+		
+	}
+	
 }
